d91ec7c034e08b7e7ec0907e9b7c10399467a675
devAmulya <amulyagupta57121@gmail.com>
2026-05-29 01:02:00 +0530

    updated axios.ts for frontend deployment

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-leads-dashboard-ly7f.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
